b25be39d5c40d6462c521c6dec690df406b2d1c0
Shahzil Shahzad <silentboys1117@gmail.com>
2025-07-28 17:43:29 +0500

    Update InventoryCard.tsx

--- src/components/stock/InventoryCard.tsx
+++ src/components/stock/InventoryCard.tsx
@@ -14,15 +14,15 @@ interface InventoryCardProps {
 // Inventory Card Component
 const InventoryCard: React.FC<InventoryCardProps> = ({ title, value, icon: Icon, iconBgColor, iconColor, situation, status }) => {
   return (
-    <div className="bg-gradient-to-b from-zinc-100 to-white dark:from-orange-500/20 dark:via-white/5 dark:to-blue-500/20 rounded-xl shadow-lg p-5 group flex items-center justify-between border border-gray-200 dark:border-zinc-900 transition-all duration-200 hover:scale-[1.02] cursor-pointer">
+    <div className="inset-0 bg-white/90 dark:bg-black/80 backdrop-blur-md rounded-xl shadow-lg p-5 group flex items-center justify-between border border-gray-200 dark:border-zinc-900 transition-all duration-200 hover:scale-[1.02] cursor-pointer">
       <div>
         <p className="text-gray-600 dark:text-zinc-200 text-sm">{title}</p>
-        <p className={`text-2xl font-bold`}>{value}</p>
+        <p className={`text-lg font-bold`}>{value}</p>
         <p>{situation}</p>
-        <p className={`font-bold ${status == "Healthy" ? "text-green-400" : "text-red-400"}`}>{status}</p>
+        <p className={`font-bold text-xs ${status == "Healthy" ? "text-green-400" : "text-red-400"}`}>{status}</p>
       </div>
       <div className={`p-3 rounded-2xl ${iconBgColor}`}>
-        <Icon size={24} className={iconColor} />
+        <Icon size={18} className={iconColor} />
       </div>
     </div>
   );
