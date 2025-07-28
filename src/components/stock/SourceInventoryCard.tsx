d6e67f5aa03928c3dbba7b09529f2cdc95b0a4c8
Shahzil Shahzad <silentboys1117@gmail.com>
2025-07-28 17:38:23 +0500

    Update SourceInventoryCard.tsx

--- src/components/stock/SourceInventoryCard.tsx
+++ src/components/stock/SourceInventoryCard.tsx
@@ -15,7 +15,7 @@ interface InventoryCardProps {
 // Inventory Card Component (reused for consistent styling across stock pages)
 const SourceInventoryCard: React.FC<InventoryCardProps> = ({ title, value, trend, icon: Icon, iconBgColor, iconColor, situation, status }) => {
   return (
-    <div className="bg-gradient-to-b from-zinc-100 to-white dark:from-orange-500/20 dark:via-white/5 dark:to-blue-500/20 rounded-xl shadow-lg p-5 group flex items-center justify-between border border-gray-200 dark:border-zinc-900 transition-all duration-200 hover:scale-[1.02] cursor-pointer">
+    <div className="inset-0 bg-white/90 dark:bg-black/80 backdrop-blur-md rounded-xl shadow-lg p-5 group flex items-center justify-between border border-gray-200 dark:border-zinc-900 transition-all duration-200 hover:scale-[1.02] cursor-pointer">
       <div>
         <p className="text-gray-600 dark:text-zinc-200 text-sm">{title}</p>
         <p className={`text-lg font-bold text-zinc-900 dark:text-white`}>{value}</p>
@@ -25,7 +25,7 @@ const SourceInventoryCard: React.FC<InventoryCardProps> = ({ title, value, trend
             {trend}
           </p>
         )}
-        <p className={`font-bold ${status === "Healthy" ? "text-green-400" : status === "Critical" ? "text-red-400" : "text-orange-400"}`}>{status}</p>
+        <p className={`font-bold text-xs ${status === "Healthy" ? "text-green-400" : status === "Critical" ? "text-red-400" : "text-orange-400"}`}>{status}</p>
       </div>
       <div className={`p-3 rounded-2xl ${iconBgColor}`}>
         <Icon size={18} className={iconColor} />
